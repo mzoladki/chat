7899a6675e9fe9d559733901307a727ece35464a
marcin <zoladkiewiczmarcin@gmail.com>
2019-01-10 09:57:24 +0100

    project done

---
+++ ChatApp/app/src/main/java/com/example/marcin/chatapp/Model/User.java
@@ -0,0 +1,47 @@
+package com.example.marcin.chatapp.Model;
+
+import com.google.android.gms.common.internal.safeparcel.SafeParcelable;
+
+import java.lang.reflect.Constructor;
+
+public class User {
+
+    private String id;
+    private String username;
+    private String imageURL;
+
+    public User(String id, String username, String imageURL){
+        this.id = id;
+        this.username = username;
+        this.imageURL = imageURL;
+    }
+
+    public User(){
+
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+
+}
